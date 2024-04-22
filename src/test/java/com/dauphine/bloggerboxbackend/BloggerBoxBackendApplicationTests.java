b237a671b66b8b40c1bef8d93f3e20c6ecc7d3d4
chirine-haddad <chirine.haddad@dauphine.eu>
2024-04-22 14:44:50 +0200

    COMMIT_MESSAGE

---
+++ src/test/java/com/dauphine/bloggerboxbackend/BloggerBoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dauphine.bloggerboxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerBoxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
