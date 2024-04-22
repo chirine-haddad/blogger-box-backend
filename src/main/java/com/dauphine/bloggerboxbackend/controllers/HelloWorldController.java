9d69bc3a8632343abb9d5064221db4cc39db3096
chirine-haddad <chirine.haddad@dauphine.eu>
2024-04-22 15:15:44 +0200

    message

---
+++ src/main/java/com/dauphine/bloggerboxbackend/controllers/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.dauphine.bloggerboxbackend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping ("hello-world")
+    public String helloWorld() {
+        return "Hello World!" ;
+    }
+
+    @GetMapping("hello")
+    public String helloByName(@RequestParam String name){
+        return "Hello " + name;
+    }
+
+    @GetMapping("hello/{name}")
+    public String hello(@PathVariable String name){
+        return "Hello " + name;
+    }
+}
