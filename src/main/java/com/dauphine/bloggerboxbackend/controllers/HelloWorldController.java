3f48c2de2b3b7d11ec5e29a91b69781fcc3dd8de
chirine-haddad <chirine.haddad@dauphine.eu>
2024-06-11 20:37:33 +0200

    premier commit

--- src/main/java/com/dauphine/bloggerboxbackend/controllers/HelloWorldController.java
+++ src/main/java/com/dauphine/bloggerboxbackend/controllers/HelloWorldController.java
@@ -1,11 +1,16 @@
 package com.dauphine.bloggerboxbackend.controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(
+        name = "Hello world API",
+        description = "My first hello world endpoints"
+)
 public class HelloWorldController {
     @GetMapping ("hello-world")
     public String helloWorld() {
