1a4f53fda60c10bea39b4d00378766e86b63f984
chirine-haddad <chirine.haddad@dauphine.eu>
2024-06-11 21:11:07 +0200

    ajout commentaires

--- src/main/java/com/dauphine/bloggerboxbackend/controllers/HelloWorldController.java
+++ src/main/java/com/dauphine/bloggerboxbackend/controllers/HelloWorldController.java
@@ -1,10 +1,13 @@
 package com.dauphine.bloggerboxbackend.controllers;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 @Tag(
@@ -23,7 +26,13 @@ public class HelloWorldController {
     }
 
     @GetMapping("hello/{name}")
-    public String hello(@PathVariable String name){
+    @Operation(
+            summary = "Hello by name endpoint",
+            description = "Returns 'Hello {name} by path variable "
+    )
+    public String hello(
+            @Parameter(description = "Name to greet")
+            @PathVariable String name){
         return "Hello " + name;
     }
 }
